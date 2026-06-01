cd0a102b73af9df8da593da130b636dc72014f19
Ritesh <riteshkr7578@gmail.com>
2026-06-01 20:21:46 +0530

    deploy

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://blogsphere-full-stack-content-management.onrender.com/api"
 });
 
 api.interceptors.request.use((req) => {
